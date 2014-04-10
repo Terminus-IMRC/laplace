85dd824570100b7d90308a1204090777b52715ba
Terminus <i.can.speak.c.and.basic@gmail.com>
2014-04-10 20:11:03 +0900

    fix for loop to specify collapse()

--- laplace.c
+++ laplace.c
@@ -22,7 +22,7 @@ void laplace()
 		fc=turn%2;
 		fn=(fc+1)%2;
 		field_changed=0;
-#pragma omp parallel for schedule(static) shared(field_changed) private(i,j)
+#pragma omp parallel for schedule(static) shared(field_changed) private(i,j) collapse(2)
 		for(i=1; i<1+M; i++)
 			for(j=1; j<1+N; j++){
 				field[fn][i][j]=(field[fc][i-1][j]+field[fc][i][j-1]+field[fc][i+1][j]+field[fc][i][j+1])/4;
